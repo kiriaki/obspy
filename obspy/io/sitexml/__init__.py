b94a3dd8b61e69461fd288f141227f4300a6bf42
Kiriaki Konstantinidou <kiriaki@itsak.gr>
2026-09-04 17:14:14 +0300

    Added imports for public functions from modules sitexml and tabular.

--- obspy/io/sitexml/__init__.py
+++ obspy/io/sitexml/__init__.py
@@ -12,5 +12,10 @@ obspy.io.sitexml - SiteXML read and write support for ObsPy
     (https://www.gnu.org/copyleft/lesser.html)
 """
 
+from .sitexml import (read_sitexml, write_sitexml, validate_sitexml,
+                      sitexml_to_sitedict, sitedict_to_sitexml,
+                      add_sitexml_reference)
+from .tabular import (csv_to_sera_site, excel_to_sera_site,
+                      add_velocity_profiles)
 from .util import (SiteXMLError, SiteXMLIOError, SiteXMLImportError,
                    SiteXMLValidationError)
